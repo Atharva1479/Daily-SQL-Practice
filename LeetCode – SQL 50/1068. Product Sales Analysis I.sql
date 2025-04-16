f8982f8a4936273afe819da4a95fa23afec38518
Atharva Jamdar <76987608+Atharva1479@users.noreply.github.com>
2025-04-16 23:40:01 +0530

    Update 1068. Product Sales Analysis I.sql

--- LeetCode – SQL 50/1068. Product Sales Analysis I.sql
+++ LeetCode – SQL 50/1068. Product Sales Analysis I.sql
@@ -68,7 +68,7 @@ From sale_id = 1, we can conclude that Nokia was sold for 5000 in the year 2008.
 From sale_id = 2, we can conclude that Nokia was sold for 5000 in the year 2009.
 From sale_id = 7, we can conclude that Apple was sold for 9000 in the year 2011.
 */
-
+/* Query*/
 SELECT Product.product_name, Sales.year, Sales.price
 FROM Sales
 LEFT JOIN Product
